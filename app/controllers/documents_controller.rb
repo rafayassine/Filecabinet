ff3630323902f55f01f1b025259f7fea274d6ca0
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-02 16:12:12 +0000

    Add documents views and define actions

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,23 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
 
   def index
+    @documents = Document.all.order("created_at DESC")
   end
 
   def create
+    @document = Document.new(doc_params)
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   def new
+    @document = Document.new
   end
+
   def edit
   end
 
@@ -19,4 +29,15 @@ class DocumentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def find_document
+      @document = Document.find(params[:id])
+    end
+
+    def doc_params
+      params.require(:document).permit(:title, :content)
+    end
+
 end
